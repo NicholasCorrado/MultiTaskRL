159e3307c40d9fc12551fefdf53ad81f1e9a7867
Nicholas Corrado <ncorrado@wisc.edu>
2025-04-07 16:05:10 -0500

    update chtc

--- chtc/job.sh
+++ chtc/job.sh
@@ -12,14 +12,14 @@ cp /staging/ncorrado/${CODENAME}.tar.gz .
 tar -xzf ${CODENAME}.tar.gz
 rm ${CODENAME}.tar.gz
 
-cd MultiTaskRL
-export PYTHONPATH=custom-envs:$PYTHONPATH # pip install -e fails on chtc because we don't have admin privileges .
-mkdir gymnasium_local
-python -m pip install 'gymnasium[mujoco]==0.29.1' --target gymnasium_local
+
+cd ${CODENAME}
+mkdir local
+python -m pip install -r requirements.txt --target local
+python -m pip install 'gymnasium[mujoco]==0.29.1' --target local
 python -m pip install 'mujoco==2.3.5' --target mujoco_local
-export PYTHONPATH=gymnasium_local:$PYTHONPATH
-export PYTHONPATH=mujoco_local:$PYTHONPATH
-$cmd
+export PYTHONPATH=local:$PYTHONPATH
+export PYTHONPATH=custom-envs:$PYTHONPATH # pip install -e fails on chtc because we don't have admin privileges .
 
 # compress results. This file will be transferred to your submit node upon job completion.
 tar czvf results_${pid}.tar.gz results
