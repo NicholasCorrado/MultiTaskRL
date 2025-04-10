054fb36e6ddc47ac88e6620899688077610b63e3
Wenyuan Huang <157926136+whuang369@users.noreply.github.com>
2025-04-10 14:32:28 -0500

    update job.sh

--- chtc/job.sh
+++ chtc/job.sh
@@ -8,7 +8,7 @@ echo $cmd
 
 # fetch your code from /staging/
 CODENAME=MultiTaskRL
-cp /staging/ncorrado/${CODENAME}.tar.gz .
+cp /staging/whuang369/${CODENAME}.tar.gz .
 tar -xzf ${CODENAME}.tar.gz
 rm ${CODENAME}.tar.gz
 
@@ -20,6 +20,7 @@ python -m pip install 'gymnasium[mujoco]==0.29.1' --target local
 python -m pip install 'mujoco==2.3.5' --target mujoco_local
 export PYTHONPATH=local:$PYTHONPATH
 export PYTHONPATH=custom-envs:$PYTHONPATH # pip install -e fails on chtc because we don't have admin privileges .
+$cmd
 
 # compress results. This file will be transferred to your submit node upon job completion.
 tar czvf results_${pid}.tar.gz results
