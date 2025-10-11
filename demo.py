a7ab225eea0a497edce46ba93616f8c6a2ca4b50
Nicholas Corrado <ncorrado@wisc.edu>
2025-10-11 13:24:18 -0500

    clean up

--- demo.py
+++
@@ -1,9 +0,0 @@
-import os
-
-for env_id in ['Swimmer-v5', 'Hopper-v5', 'HalfCheetah-v5', 'Walker2d-v5', 'Ant-v5', 'Humanoid-v5']:
-    for seed in range(3):
-        command = (
-            f"python ppo_continuous_action.py --run-id {seed} --seed {seed} "
-            f"--env-id {env_id} --total-timesteps 20000 --eval-freq 1 "
-        )
-        os.system(command)
